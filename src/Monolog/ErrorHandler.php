bf8f3bd45d5a76870e520caa91fa4588fa65416d
Stéphane <stephane.delprat@talkspirit.com>
2025-12-19 15:47:37 +0900

    Add support for PHP 8.4: Remove E_STRICT

--- src/Monolog/ErrorHandler.php
+++ src/Monolog/ErrorHandler.php
@@ -120,7 +120,6 @@ class ErrorHandler
             E_USER_ERROR        => LogLevel::ERROR,
             E_USER_WARNING      => LogLevel::WARNING,
             E_USER_NOTICE       => LogLevel::NOTICE,
-            E_STRICT            => LogLevel::NOTICE,
             E_RECOVERABLE_ERROR => LogLevel::ERROR,
             E_DEPRECATED        => LogLevel::NOTICE,
             E_USER_DEPRECATED   => LogLevel::NOTICE,
